207e32e25aa6d524e120cb420fbe5692c8a40950
yassineACE <yassinemedmoum453@gmail.com>
2023-09-16 13:17:10 +0100

    hot

---
+++ Untitled4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (){
+
+float letat ;
+printf("entre letat svp");
+scanf("%f",&letat);
+if (letat < 0)
+    printf("letat est lbaard");
+    else if
+        (letat>100)
+        printf("letat est HOT");
+    else
+        printf("letat est normal");
+
+}
+
